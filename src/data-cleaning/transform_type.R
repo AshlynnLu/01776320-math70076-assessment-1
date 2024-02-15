b214024c56dadde7c4b2f4eac46c1ba070b5993d
AshlynnLu <lkx.975@gmail.com>
2024-02-15 16:00:07 +0000

    derived data saved

--- src/data-cleaning/transform_type.R
+++ src/data-cleaning/transform_type.R
@@ -1,21 +1,28 @@
+# Import packages
 library(dplyr)
 
 # Load raw data
-data <- read.csv("~/Desktop/MSc/spring/DS/01776320-math70076-assessment-1/data/raw/Marijuana_Sales_By_County_2014_To_Date_Report_County_Report.csv",
+data_raw <- read.csv("~/Desktop/MSc/spring/DS/01776320-math70076-assessment-1/data/raw/Marijuana_Sales_By_County_2014_To_Date_Report_County_Report.csv",
                  skip = 4, header = TRUE)
-data <- data[1:5035, ]
+data_raw <- data[1:5035, ]
 
 # Rename the header
-colnames(data) <- c("Month", "Year", "County", "Medical", "Retail")
+colnames(data_raw) <- c("Month", "Year", "County", "Medical", "Retail")
 
 # Transform the sales into integer
-data <- data %>%
+data <- data_raw %>%
   mutate(Medical = ifelse(Medical != "NR" & !is.na(Medical),
                           as.integer(gsub("\\$|,", "", Medical)),
                           Medical))
-data <- data %>%
+data <- data_raw %>%
   mutate(Retail = ifelse(Retail != "NR" & !is.na(Retail),
-                          as.integer(gsub("\\$|,", "", Retail)),
-                          Retail))
+                         as.integer(gsub("\\$|,", "", Retail)),
+                         Retail))
+# Remove NR and NA
+data_non_NR_NA <- data_raw %>%
+  filter(Medical != "NR" & !is.na(Medical) & Retail != "NR" & !is.na(Retail)) %>%
+  mutate(Medical = as.integer(gsub("\\$|,", "", Medical)),
+         Retail = as.integer(gsub("\\$|,", "", Retail)))
 
+save(data_non_NR_NA, file = "data_non_NR_NA.RData")
 
