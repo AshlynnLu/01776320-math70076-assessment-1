f2b6c7b67027bce62ec9417ce718c557c1bb7c01
AshlynnLu <lkx.975@gmail.com>
2024-02-19 21:41:40 +0000

    font

---
+++ src/Font/import-font.R
@@ -0,0 +1,15 @@
+library(extrafont)
+library(showtext)
+
+font_add(family = "Econ Sans Cnd",
+         regular = "econsanscndreg-webfont.woff.ttf",
+         bold = "econsans-subset-cd-bd.woff.ttf")
+font_add(family = "Econ Sans Cnd Light",
+         "econsans-subset-cd-li.woff.ttf")
+font_add(family = "Econ Sans Cnd Medium",
+         "econsans-subset-cd-md.woff.ttf")
+font_add(family = "Econ Sans Cnd Bold",
+         "econsans-condensed-primary-subset-bd.ttf")
+
+loadfonts()
+
